88615fb5d0b37e9e4a6462a84ce32a1e09282dde
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-01-08 12:15:42 +0100

    allow main

--- docs/autogen_config.py
+++ docs/autogen_config.py
@@ -13,6 +13,10 @@ at the command line when you start it.
 
 destination = os.path.join(os.path.dirname(__file__), 'source/config_options.rst')
 
-with open(destination, 'w') as f:
-    f.write(header)
-    f.write(JupyterQtConsoleApp().document_config_options())
+def main():
+    with open(destination, 'w') as f:
+        f.write(header)
+        f.write(JupyterQtConsoleApp().document_config_options())
+
+if __name__ == '__main__':
+    main()
