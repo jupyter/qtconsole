e69f0cd3638c10fa4e3abf00e3337dde9c757227
Carlos Cordoba <ccordoba12@gmail.com>
2016-02-21 03:17:41 -0500

    Improve gui_reference text
    - This follows @takluyver and @willingc review.

--- qtconsole/usage.py
+++ qtconsole/usage.py
@@ -12,9 +12,10 @@ The Jupyter QtConsole
 =====================
 
 This console is designed to emulate the look, feel and workflow of a terminal
-environment, while adding a number of enhancements that are simply not possible
-in a real terminal, such as inline syntax highlighting, true multiline editing,
-inline graphics and much more.
+environment. Beyond this basic design, the console also implements
+functionality not currently found in most terminal emulators. Some examples of
+these console enhancements are inline syntax highlighting, multiline editing,
+inline graphics, and others.
 
 This quick reference document contains the basic information you'll need to
 know to make the most efficient use of it.  For the various command line
@@ -27,19 +28,19 @@ Multiline editing
 
 The graphical console is capable of true multiline editing, but it also tries
 to behave intuitively like a terminal when possible.  If you are used to
-IPython's old terminal behavior, you should find the transition painless, and
-once you learn a few basic keybindings it will be a much more efficient
-environment.
+IPython's old terminal behavior, you should find the transition painless. If
+you learn to use a few basic keybindings, the console provides even greater
+efficiency.
 
 For single expressions or indented blocks, the console behaves almost like the
-terminal IPython: single expressions are immediately evaluated, and indented
+IPython terminal: single expressions are immediately evaluated, and indented
 blocks are evaluated once a single blank line is entered::
 
-    In [1]: print "Hello Jupyter!"  # Enter was pressed at the end of the line
+    In [1]: print ("Hello Jupyter!")  # Enter was pressed at the end of the line
     Hello Jupyter!
 
-    In [2]: for i in range(10):
-       ...:     print i,
+    In [2]: for num in range(10):
+       ...:     print(num)
        ...:
     0 1 2 3 4 5 6 7 8 9
 
@@ -47,13 +48,13 @@ If you want to enter more than one expression in a single input block
 (something not possible in the terminal), you can use ``Control-Enter`` at the
 end of your first line instead of ``Enter``.  At that point the console goes
 into 'cell mode' and even if your inputs are not indented, it will continue
-accepting arbitrarily many lines until either you enter an extra blank line or
+accepting lines until either you enter an extra blank line or
 you hit ``Shift-Enter`` (the key binding that forces execution).  When a
 multiline cell is entered, the console analyzes it and executes its code producing
 an ``Out[n]`` prompt only for the last expression in it, while the rest of the
 cell is executed as if it was a script.  An example should clarify this::
 
-    In [3]: x=1  # Hit C-Enter here
+    In [3]: x=1  # Hit Ctrl-Enter here
        ...: y=2  # from now on, regular Enter is sufficient
        ...: z=3
        ...: x**2  # This does *not* produce an Out[] value
@@ -75,11 +76,11 @@ text editor.  When you want it executed, the simplest thing to do is to hit the
 force execution key, ``Shift-Enter`` (though you can also navigate to the end
 and append a blank line by using ``Enter`` twice).
 
-If you've edited a multiline cell and accidentally navigate out of it with the
-up or down arrow keys, the console will clear the cell and replace it with the
-contents of the one above or below that you navigated to.  If this was an
-accident and you want to retrieve the cell you were editing, use the Undo
-keybinding, ``Control-z``.
+If you are editing a multiline cell and accidentally navigate out of it using the
+up or down arrow keys, the console clears the cell and replaces it with the
+contents of the cell which the up or down arrow key stopped on.  If you wish to
+to undo this action,  perhaps because of an accidental keypress, use the Undo
+keybinding, ``Control-z``, to restore the original cell.
 
 
 Key bindings
@@ -88,65 +89,59 @@ Key bindings
 The Jupyter QtConsole supports most of the basic Emacs line-oriented keybindings,
 in addition to some of its own.
 
-The keybinding prefixes mean:
-
-- ``C``: Control
-- ``S``: Shift
-- ``M``: Meta (typically the Alt key)
-
 The keybindings themselves are:
 
 - ``Enter``: insert new line (may cause execution, see above).
-- ``C-Enter``: *force* new line, *never* causes execution.
-- ``S-Enter``: *force* execution regardless of where cursor is, no newline added.
+- ``Ctrl-Enter``: *force* new line, *never* causes execution.
+- ``Shift-Enter``: *force* execution regardless of where cursor is, no newline added.
 - ``Up``: step backwards through the history.
 - ``Down``: step forwards through the history.
-- ``S-Up``: search backwards through the history (like ``C-r`` in bash).
-- ``S-Down``: search forwards through the history.
-- ``C-c``: copy highlighted text to clipboard (prompts are automatically stripped).
-- ``C-S-c``: copy highlighted text to clipboard (prompts are not stripped).
-- ``C-v``: paste text from clipboard.
-- ``C-z``: undo (retrieves lost text if you move out of a cell with the arrows).
-- ``C-S-z``: redo.
-- ``C-o``: move to 'other' area, between pager and terminal.
-- ``C-l``: clear terminal.
-- ``C-a``: go to beginning of line.
-- ``C-e``: go to end of line.
-- ``C-u``: kill from cursor to the begining of the line.
-- ``C-k``: kill from cursor to the end of the line.
-- ``C-y``: yank (paste)
-- ``C-p``: previous line (like up arrow)
-- ``C-n``: next line (like down arrow)
-- ``C-f``: forward (like right arrow)
-- ``C-b``: back (like left arrow)
-- ``C-d``: delete next character, or exits if input is empty
-- ``M-<``: move to the beginning of the input region.
-- ``M->``: move to the end of the input region.
-- ``M-d``: delete next word.
-- ``M-Backspace``: delete previous word.
-- ``C-.``: force a kernel restart (a confirmation dialog appears).
-- ``C-+``: increase font size.
-- ``C--``: decrease font size.
-- ``C-M-Space``: toggle full screen. (Command-Control-Space on Mac OS X)
+- ``Shift-Up``: search backwards through the history (like ``Control-r`` in bash).
+- ``Shift-Down``: search forwards through the history.
+- ``Control-c``: copy highlighted text to clipboard (prompts are automatically stripped).
+- ``Control-Shift-c``: copy highlighted text to clipboard (prompts are not stripped).
+- ``Control-v``: paste text from clipboard.
+- ``Control-z``: undo (retrieves lost text if you move out of a cell with the arrows).
+- ``Control-Shift-z``: redo.
+- ``Control-o``: move to 'other' area, between pager and terminal.
+- ``Control-l``: clear terminal.
+- ``Control-a``: go to beginning of line.
+- ``Control-e``: go to end of line.
+- ``Control-u``: kill from cursor to the begining of the line.
+- ``Control-k``: kill from cursor to the end of the line.
+- ``Control-y``: yank (paste)
+- ``Control-p``: previous line (like up arrow)
+- ``Control-n``: next line (like down arrow)
+- ``Control-f``: forward (like right arrow)
+- ``Control-b``: back (like left arrow)
+- ``Control-d``: delete next character, or exits if input is empty
+- ``Alt-<``: move to the beginning of the input region.
+- ``alt->``: move to the end of the input region.
+- ``Alt-d``: delete next word.
+- ``Alt-Backspace``: delete previous word.
+- ``Control-.``: force a kernel restart (a confirmation dialog appears).
+- ``Control-+``: increase font size.
+- ``Control--``: decrease font size.
+- ``Control-Alt-Space``: toggle full screen. (Command-Control-Space on Mac OS X)
 
 The pager
 =========
 
 The Jupyter QtConsole will show long blocks of text from many sources using a
-builtin pager. You can control where this pager appears with the ``--paging``
+built-in pager. You can control where this pager appears with the ``--paging``
 command-line flag:
 
 - ``inside`` [default]: the pager is overlaid on top of the main terminal. You
   must quit the pager to get back to the terminal (similar to how a pager such
-  as ``less`` or ``more`` works).
+  as ``less`` or ``more`` pagers behave).
 
-- ``vsplit``: the console is made double-tall, and the pager appears on the
+- ``vsplit``: the console is made double height, and the pager appears on the
   bottom area when needed.  You can view its contents while using the terminal.
 
-- ``hsplit``: the console is made double-wide, and the pager appears on the
+- ``hsplit``: the console is made double width, and the pager appears on the
   right area when needed.  You can view its contents while using the terminal.
 
-- ``none``: the console never pages output.
+- ``none``: the console displays output without paging.
 
 If you use the vertical or horizontal paging modes, you can navigate between
 terminal and pager as follows:
@@ -169,7 +164,8 @@ limitation is that you can *not* interact back with the subprocess, so anything
 that invokes a pager or expects you to type input into it will block and hang
 (you can kill it with ``Control-C``).
 
-We have provided as magics ``%less`` to page files (aliased to ``%more``),
+The console can use magics provided by the IPython kernel. These magics include
+``%less`` to page files (aliased to ``%more``),
 ``%clear`` to clear the terminal, and ``%man`` on Linux/OSX.  These cover the
 most common commands you'd want to call in your subshell and that would cause
 problems if invoked via ``!cmd``, but you need to be aware of this limitation.
@@ -177,9 +173,9 @@ problems if invoked via ``!cmd``, but you need to be aware of this limitation.
 Display
 =======
 
-The Jupyter QtConsole can now display objects in a variety of formats, including
-PNG and SVG. This is accomplished using the display functions in
-``IPython.display``::
+For example, if using the IPython kernel, there are functions available for
+object display:
+
 
     In [4]: from IPython.display import display
 
@@ -199,17 +195,4 @@ can also be given custom formatter functions for various types::
     In [8]: png_formatter.for_type(Foo, foo_to_png)
 
 For further details, see ``IPython.core.formatters``.
-
-Inline matplotlib graphics
-==========================
-
-The Jupyter QtConsole is capable of displaying matplotlib figures inline, in SVG
-or PNG format.  If started with the ``matplotlib=inline``, then all figures are
-rendered inline automatically (PNG by default).  If started with ``--matplotlib``
-or ``matplotlib=<your backend>``, then a GUI backend will be used, but IPython's
-``display()`` and ``getfigs()`` functions can be used to view plots inline::
-
-    In [9]: display(*getfigs())    # display all figures inline
-
-    In[10]: display(*getfigs(1,2)) # display figures 1 and 2 inline
 """
