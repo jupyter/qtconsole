32e0fcee465a22a8407f210e04f9a210d8f0c576
Thomas Kluyver <thomas@kluyver.me.uk>
2017-01-07 14:24:25 +0000

    Add dependency on ipython_genutils
    Closes gh-180

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ if 'develop' in sys.argv or any(a.startswith('bdist') for a in sys.argv):
 setuptools_args = {}
 install_requires = setuptools_args['install_requires'] = [
     'traitlets',
+    'ipython_genutils',
     'jupyter_core',
     'jupyter_client>=4.1',
     'pygments',
