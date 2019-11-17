8b05b32ea9806c74e3c0dda89968c44b5f9e629b
dalthviz <d.althviz10@uniandes.edu.co>
2019-11-17 13:17:02 -0500

    Tests: Update comms and console_widget tests

--- qtconsole/tests/test_comms.py
+++ qtconsole/tests/test_comms.py
@@ -3,6 +3,7 @@ import sys
 
 import unittest
 
+from ipython_genutils.py3compat import PY3
 from jupyter_client.blocking.channels import Empty
 
 from qtconsole.manager import QtKernelManager
@@ -142,13 +143,20 @@ class Tests(unittest.TestCase):
         # Get close
         comm.close('close')
         msg = self._get_next_msg()
-        # For some reason ipykernel notifies me that it is closing,
-        # even though I closed the comm
-        assert msg['header']['msg_type'] == 'comm_close'
-        assert comm.comm_id == msg['content']['comm_id']
-        msg = self._get_next_msg()
-        assert msg['header']['msg_type'] == 'stream'
-        assert msg['content']['text'] == 'close\n'
+
+        # Recived message has a header and parent header. The parent header has
+        # the info about the close message type in Python 3
+        if PY3:
+            assert msg['parent_header']['msg_type'] == 'comm_close'
+            assert msg['msg_type'] == 'stream'
+            assert msg['content']['text'] == 'close\n'
+        else:
+            # For some reason ipykernel notifies me that it is closing,
+            # even though I closed the comm
+            assert msg['header']['msg_type'] == 'comm_close'
+            assert comm.comm_id == msg['content']['comm_id']
+            msg = self._get_next_msg()
+            assert msg['header']['msg_type'] == 'stream'
 
 if __name__ == "__main__":
     unittest.main()
