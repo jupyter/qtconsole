6f70a046520b7335091e0a47b1e0898929487410
jsbautista <js.bautista@uniandes.edu.co>
2024-09-29 16:21:37 -0500

    shortcuts frontend

--- qtconsole/frontend_widget.py
+++ qtconsole/frontend_widget.py
@@ -13,13 +13,12 @@ from qtpy import QtCore, QtGui, QtWidgets
 from qtconsole.base_frontend_mixin import BaseFrontendMixin
 from traitlets import Any, Bool, Instance, Unicode, DottedObjectName, default
 from traitlets.config.configurable import LoggingConfigurable
-from traitlets import Enum, Integer
-from traitlets import HasTraits, observe
+from traitlets import Enum, Integer, observe
 from .bracket_matcher import BracketMatcher
 from .call_tip_widget import CallTipWidget
 from .history_console_widget import HistoryConsoleWidget
 from .pygments_highlighter import PygmentsHighlighter
-from .util import import_item, shortcut_manager
+from .util import import_item
 
 
 class FrontendHighlighter(PygmentsHighlighter):
@@ -145,6 +144,8 @@ class FrontendWidget(HistoryConsoleWidget, BaseFrontendMixin):
         lexer_class = import_item(self.lexer_class)
         return lexer_class()
 
+    shortcut_copy_raw = Unicode('Ctrl+Shift+C').tag(config=True)
+
     # Emitted when a user visible 'execute_request' has been submitted to the
     # kernel from the FrontendWidget. Contains the code to be executed.
     executing = QtCore.Signal(object)
@@ -193,13 +194,12 @@ class FrontendWidget(HistoryConsoleWidget, BaseFrontendMixin):
         # Configure actions.
         action = self._copy_raw_action
         action.setEnabled(False)
-        action.setShortcut(shortcut_manager.shortcut_copy_raw)
+        action.setShortcut(self.shortcut_copy_raw)
         action.setShortcutContext(QtCore.Qt.WidgetWithChildrenShortcut)
         action.triggered.connect(self.copy_raw)
         self.copy_available.connect(action.setEnabled)
         self.addAction(action)
         self.copy_raw_action = action
-        shortcut_manager.observe(self.update_shortcuts, names=['shortcut_copy_raw'])
 
         # Connect signal handlers.
         document = self._control.document()
@@ -215,6 +215,7 @@ class FrontendWidget(HistoryConsoleWidget, BaseFrontendMixin):
     # 'ConsoleWidget' public interface
     #---------------------------------------------------------------------------
 
+    @observe('shortcut_copy_raw')
     def update_shortcuts(self, change):
         if change['name'] == 'shortcut_copy_raw':
             self.copy_raw_action.setShortcut(change['new'])
